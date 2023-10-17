c2cbfa4c6250a7d19b51b0371cfacdcef25a7197
GuoHao <guohao3510@foxmail.com>
2023-10-17 14:45:43 +0800

    vector and deque no tests

---
+++ include/gbc_deque.h
@@ -0,0 +1,369 @@
+#ifndef _GBC_DQUEUE_H
+#define _GBC_DQUEUE_H
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_DQ_SIZE 8
+
+/// @brief vector double-ended queue
+typedef struct _vdq_t vdq_t;
+
+/// @brief create a new vdq_t
+/// @param element_size: the size of each element
+vdq_t *vdq_new(size_t element_size);
+
+/// @brief drop the vdq_t out of memory
+/// @param q
+/// @return return false if the drop failed
+bool vdq_drop(vdq_t *q);
+
+/// @brief check if vdq_t is empty
+/// @param q
+/// @return
+bool vdq_is_empty(const vdq_t *q);
+
+/// @brief check if vdq_t is full
+/// @param
+/// @return
+bool vdq_is_full(const vdq_t *);
+
+/// @brief get the length of vdq_t
+/// @param q
+/// @return
+size_t vdq_length(const vdq_t *q);
+
+/// @brief push an element at the back of vdq_t
+/// @param q
+/// @param vlaue
+/// @return
+bool vdq_push_back(vdq_t *q, const void *value);
+
+/// @brief push an element at the front of vdq_t
+/// @param q
+/// @param value
+/// @return
+bool vdq_push_front(vdq_t *q, const void *value);
+
+/// @brief insert an element in the vdq_t given an index
+/// @param q
+/// @param idx
+/// @param value
+/// @return
+bool vdq_insert(vdq_t *q, size_t idx, const void *value);
+
+/// @brief check the front/first element in the vdq_t
+/// @param q
+/// @return
+const void *vdq_front(const vdq_t *q);
+
+/// @brief check the back/last element in the vdq_t
+/// @param q
+/// @return
+const void *vdq_back(const vdq_t *q);
+
+/// @brief check the element with a certain index
+/// @param q
+/// @param idx
+/// @return
+const void *vdq_at(const vdq_t *q, size_t idx);
+
+/// @brief update the value in the vdq_t with an index
+/// @param q
+/// @param idx
+/// @param value
+/// @return
+bool vdq_update(vdq_t *q, size_t idx, const void *value);
+
+/// @brief for each element in the vdq_t and process it
+/// @param q
+/// @param foreach_fn
+void vdq_foreach(const vdq_t *q, void (*foreach_fn)(const void *));
+
+/// @brief delete the first element
+/// @param q
+/// @return return false if the deletion failed
+bool vdq_del_front(vdq_t *q);
+
+/// @brief delete the last element
+/// @param q
+/// @return return false if the deletion failed
+bool vdq_del_back(vdq_t *q);
+
+/// @brief delete an element in the vdq_t with an index
+/// @param q
+/// @param idx
+/// @return
+bool vdq_del(vdq_t *q, size_t idx);
+
+/// @brief create a new vdq_t with specified capacity size
+/// @param element_size
+/// @param cap
+/// @return
+vdq_t *vdq_new_with_cap(size_t element_size, size_t cap);
+
+/// @brief deep clone a vdq_t
+/// @param q
+/// @return
+vdq_t *vdq_clone(vdq_t *q);
+
+/// reverse the vdq_t
+bool vdq_reverse(vdq_t *q);
+
+/// sort the vdq_t
+bool vdq_sort(vdq_t *q, int (*cmp_fn)(const void *, const void *));
+
+typedef struct _vdq_t {
+  size_t obj_size;
+  size_t front;
+  size_t rear;
+  size_t cap;
+  size_t size;
+  char *buf;
+} vdq_t;
+
+vdq_t *vdq_new(size_t element_size) {
+  char *buf = (char *)malloc(DEFAULT_DQ_SIZE * element_size);
+  if (!buf) return NULL;
+  vdq_t *q = (vdq_t *)malloc(sizeof(vdq_t));
+  if (!q) {
+    free(buf);
+    buf = NULL;
+    return NULL;
+  }
+  q->obj_size = element_size;
+  q->front = 0;
+  q->rear = 0;
+  q->cap = DEFAULT_DQ_SIZE;
+  q->size = 0;
+  q->buf = buf;
+  return q;
+}
+
+vdq_t *vdq_new_with_cap(size_t element_size, size_t cap) {
+  char *buf = (char *)malloc(cap * element_size);
+  if (!buf) return NULL;
+  vdq_t *q = (vdq_t *)malloc(sizeof(vdq_t));
+  if (!q) {
+    free(buf);
+    buf = NULL;
+    return NULL;
+  }
+  q->obj_size = element_size;
+  q->front = 0;
+  q->rear = 0;
+  q->cap = cap;
+  q->size = 0;
+  q->buf = buf;
+  return q;
+}
+
+vdq_t *vdq_clone(vdq_t *q) {
+  if (!q) return NULL;
+  vdq_t *out = vdq_new_with_cap(q->obj_size, q->cap);
+  if (!out) return NULL;
+  memcpy(out->buf, q->buf, q->obj_size * q->size);
+  out->front = q->front;
+  out->rear = q->rear;
+  out->size = q->size;
+  return out;
+}
+
+bool vdq_drop(vdq_t *q) {
+  if (!q) return false;
+  if (q->buf) {
+    free(q->buf);
+    q->buf = NULL;
+  }
+  free(q);
+  q = NULL;
+  return true;
+}
+
+bool vdq_is_empty(const vdq_t *q) {
+  assert(q);
+  return q->size == 0;
+}
+
+bool vdq_is_full(const vdq_t *q) {
+  assert(q);
+  return q->size == q->cap;
+}
+
+size_t vdq_length(const vdq_t *q) {
+  assert(q);
+  return q->size;
+}
+
+static bool vdq_enlarge(vdq_t *q, size_t new_cap) {
+  char *new_buf = (char *)malloc(q->obj_size * new_cap);
+  if (!new_buf) return false;
+  if (q->rear > q->front) {
+    memcpy(new_buf, q->buf, q->obj_size * q->size);
+    q->front = 0;
+    q->rear = q->size;
+    q->cap = new_cap;
+    if (q->buf) {
+      free(q->buf);
+      q->buf = new_buf;
+    } else
+      q->buf = new_buf;
+    return true;
+  } else {
+    size_t front_parts = (q->cap - q->front) * q->obj_size;
+    size_t rear_parts = q->rear * q->obj_size;
+    memcpy(new_buf, q->buf + (q->front * q->obj_size), front_parts);
+    memcpy(new_buf + front_parts, q->buf, rear_parts);
+    q->front = 0;
+    q->rear = q->size;
+    q->cap = new_cap;
+    if (q->buf) {
+      free(q->buf);
+      q->buf = new_buf;
+    } else
+      q->buf = new_buf;
+    return true;
+  }
+}
+
+bool vdq_push_back(vdq_t *q, const void *value) {
+  assert(q && value);
+  if (vdq_is_full(q)) {
+    if (!vdq_enlarge(q, q->cap * 2)) return false;
+  }
+  memcpy(q->buf + (q->rear * q->obj_size), value, q->obj_size);
+  q->rear = (q->rear + 1) % q->cap;
+  q->size++;
+  return true;
+}
+
+bool vdq_push_front(vdq_t *q, const void *value) {
+  assert(q && value);
+  if (vdq_is_full(q)) {
+    if (!vdq_enlarge(q, q->cap * 2)) return false;
+  }
+  q->front = (q->front + q->cap - 1) % q->cap;
+  memcpy(q->buf + (q->front * q->obj_size), value, q->obj_size);
+  q->size++;
+  return true;
+}
+
+bool vdq_insert(vdq_t *q, size_t idx, const void *value) {
+  assert(q && value && q->size > idx);
+  if (vdq_is_full(q)) {
+    if (!vdq_enlarge(q, q->cap * 2)) return false;
+  } else {
+    if (!vdq_enlarge(q, q->cap)) return false;
+  }
+  if (idx == 0)
+    return vdq_push_front(q, value);
+  else if (idx == q->size)
+    return vdq_push_back(q, value);
+  else {
+    size_t pos0 = (q->front + idx) % q->cap;
+    size_t pos1 = (q->front + idx + 1) % q->cap;
+    memmove(q->buf + pos1 * q->obj_size, q->buf + pos0 * q->obj_size,
+            (q->size - idx) * q->obj_size);
+    memcpy(q->buf + pos0 * q->obj_size, value, q->obj_size);
+    q->size++;
+    q->rear = (q->rear + 1) % q->cap;
+    return true;
+  }
+}
+
+const void *vdq_front(const vdq_t *q) {
+  assert(q);
+  if (q->size == 0) return NULL;
+  char *ptr = q->buf + q->front * q->obj_size;
+  return ptr;
+}
+
+const void *vdq_back(const vdq_t *q) {
+  assert(q);
+  if (q->size) return NULL;
+  char *ptr = q->buf + ((q->rear + q->cap - 1) % q->cap) * q->obj_size;
+  return ptr;
+}
+
+const void *vdq_at(const vdq_t *q, size_t idx) {
+  assert(q && q->size > idx);
+  char *ptr = q->buf + ((q->front + idx) % q->cap) * q->obj_size;
+  return ptr;
+}
+
+bool vdq_update(vdq_t *q, size_t idx, const void *value) {
+  assert(q && value && q->size > idx);
+  char *ptr = q->buf + ((q->front + idx) % q->cap) * q->obj_size;
+  memcpy(ptr, value, q->obj_size);
+  return true;
+}
+
+void vdq_foreach(const vdq_t *q, void (*foreach_fn)(const void *)) {
+  for (int i = 0; i < q->size; ++i) foreach_fn(vdq_at(q, i));
+}
+
+bool vdq_del_front(vdq_t *q) {
+  assert(q);
+  if (q->size == 0) return false;
+  q->front = (q->front + 1) % q->cap;
+  q->size--;
+  return true;
+}
+
+bool vdq_del_back(vdq_t *q) {
+  assert(q);
+  if (q->size == 0) return false;
+  q->rear = (q->rear + q->cap - 1) % q->cap;
+  q->size--;
+  return true;
+}
+
+bool vdq_del(vdq_t *q, size_t idx) {
+  assert(q && q->size > idx);
+  if (idx == 0)
+    return vdq_del_front(q);
+  else if (idx == q->size)
+    return vdq_del_back(q);
+  else {
+    if (q->rear < q->front) {
+      if (!vdq_enlarge(q, q->cap)) return false;
+    }
+    memmove(q->buf + ((idx + q->front) % q->cap) * q->obj_size,
+            q->buf + ((idx + 1 + q->front) % q->cap) * q->obj_size,
+            (q->size - idx - 1) * q->obj_size);
+    q->size--;
+    q->rear = (q->rear + q->cap - 1) % q->cap;
+    return true;
+  }
+}
+
+bool vdq_reverse(vdq_t *q) {
+  assert(q);
+  if (q->size == 0) return false;
+  if (!vdq_enlarge(q, q->cap)) return false;
+  size_t start = q->front;
+  size_t end = (q->rear + q->cap - 1) % q->cap;
+  char tmp[q->obj_size];
+  while (start < end) {
+    memcpy(tmp, q->buf + start * q->obj_size, q->obj_size);
+    memcpy(q->buf + start * q->obj_size, q->buf + end * q->obj_size,
+           q->obj_size);
+    memcpy(q->buf + end * q->obj_size, tmp, q->obj_size);
+    start = (start + 1) % q->cap;
+    end = (end + q->cap - 1) % q->cap;
+  }
+  return true;
+}
+
+bool vdq_sort(vdq_t *q, int (*cmp_fn)(const void *, const void *)) {
+  assert(q);
+  if (q->size == 0) return false;
+  if (!vdq_enlarge(q, q->cap)) return false;
+  qsort(q->buf, q->size, q->obj_size, cmp_fn);
+  return true;
+}
+
+#endif
---
+++ include/gbc_vector.h
@@ -0,0 +1,255 @@
+#ifndef _GBC_VECTOR_H  // generic buffered collection
+#define _GBC_VECTOR_H
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_VEC_CAP 8
+
+typedef void *vec_data_t;
+
+/// @brief The Vector collection
+typedef struct _vec vec_t;
+
+/// @brief create a new vector
+/// @param obj_size the size of each element
+/// @return
+vec_t *vec_new(size_t obj_size);
+
+/// @brief creata a new vector with capacity
+/// @param obj_size
+/// @param cap
+/// @return
+vec_t *vec_new_with_cap(size_t obj_size, size_t cap);
+
+/// @brief drop the vector out of memory
+/// @param vec
+bool vec_drop(vec_t *);
+
+/// @brief push one element into the vector
+/// @param
+/// @param
+bool vec_push(vec_t *, const vec_data_t);
+
+/// @brief pop out the last element on the heap memory!!
+/// @param
+/// @return
+bool vec_del_top(vec_t *);
+
+/// Check the top element in the vector
+const vec_data_t vec_top(const vec_t *);
+
+/// @brief deep copy a vector
+/// @param
+/// @return
+vec_t *vec_clone(vec_t *);
+
+/// @brief copy an array an create a new vector
+/// @param _arr
+/// @param array_size
+/// @param obj_size
+/// @return
+vec_t *vec_from_array(const vec_data_t _arr, size_t array_size,
+                      size_t obj_size);
+
+/// @brief Insert an element into the vector with an index
+/// @param vec
+/// @param data
+/// @param idx
+bool vec_insert(vec_t *vec, const vec_data_t data, size_t idx);
+
+/// @brief delete an item at the input index
+/// @param vec
+/// @param idx
+/// @return
+bool vec_del_at(vec_t *vec, const size_t idx);
+
+/// @brief get the element in the vector at the input index
+/// @param vec
+/// @param idx
+/// @return
+const vec_data_t vec_at(const vec_t *vec, const size_t idx);
+
+/// @brief
+///         comparison function which returns ​a negative integer
+///         value if the first argument is less than the second,
+///         a positive integer value if the first argument is greater
+///         than the second and zero if the arguments are equivalent.
+/// @param vec
+/// @param cmp_fn
+bool vec_sort(vec_t *vec, int (*cmp_fn)(const void *, const void *));
+
+/// @brief reverse the vec_t
+/// @param vec
+/// @return
+bool vec_reverse(vec_t *vec);
+
+typedef struct _vec {
+  size_t size;
+  size_t cap;
+  size_t obj_size;
+  char *buf;
+} vec_t;
+
+vec_t *vec_new(size_t obj_size) {
+  char *buf = (char *)malloc(obj_size * DEFAULT_VEC_CAP);
+  if (!buf) return NULL;
+  vec_t *v = (vec_t *)malloc(sizeof(vec_t));
+  if (!v) {
+    free(buf);
+    buf = NULL;
+    return NULL;
+  }
+  v->buf = buf;
+  v->size = 0;
+  v->obj_size = obj_size;
+  v->cap = DEFAULT_VEC_CAP;
+  return v;
+}
+
+vec_t *vec_new_with_cap(size_t obj_size, size_t cap) {
+  char *buf = (char *)malloc(obj_size * cap);
+  if (!buf) return NULL;
+  vec_t *v = (vec_t *)malloc(sizeof(vec_t));
+  if (!v) {
+    free(buf);
+    buf = NULL;
+    return NULL;
+  }
+  v->buf = buf;
+  v->size = 0;
+  v->obj_size = obj_size;
+  v->cap = cap;
+  return v;
+}
+
+bool vec_drop(vec_t *vec) {
+  if (!vec) return false;
+  if (!vec->buf) {
+    free(vec);
+    vec = NULL;
+    return true;
+  } else {
+    free(vec->buf);
+    vec->buf = NULL;
+    free(vec);
+    vec = NULL;
+    return true;
+  }
+}
+
+static bool vec_enlarge(vec_t *vec, size_t new_cap) {
+  char *new_buf = (char *)malloc(vec->obj_size * new_cap);
+  if (!new_buf) {
+    return false;
+  }
+  vec->cap = new_cap;
+  memcpy(new_buf, vec->buf, (vec->size * vec->obj_size));
+  free(vec->buf);
+  vec->buf = new_buf;
+  return true;
+}
+
+bool vec_push(vec_t *vec, const vec_data_t data) {
+  assert(vec && data);
+  if (vec->cap == vec->size) {
+    if (!vec_enlarge(vec, vec->cap * 2)) return false;
+  }
+  memcpy(vec->buf + vec->obj_size * vec->size, data, vec->obj_size);
+  vec->size++;
+  return true;
+}
+
+bool vec_del_top(vec_t *vec) {
+  // assert(vec->obj_size == len(out_buf))
+  assert(vec);
+  if (vec->size == 0) {
+    return false;
+  }
+  vec->size--;
+  return true;
+}
+
+vec_t *vec_clone(vec_t *vec) {
+  assert(vec);
+  vec_t *v = vec_new_with_cap(vec->obj_size, vec->cap);
+  if (!v) return NULL;
+  v->size = vec->size;
+  v->cap = vec->cap;
+  v->obj_size = vec->obj_size;
+  memcpy(v->buf, vec->buf, vec->size * vec->obj_size);
+  return v;
+}
+
+vec_t *vec_from_array(const vec_data_t _arr, size_t array_size,
+                      size_t obj_size) {
+  size_t cap = (array_size % 2 == 0) ? array_size : array_size + 1;
+  vec_t *v = vec_new_with_cap(obj_size, cap);
+  if (!v) return NULL;
+  v->size = array_size;
+  memcpy(v->buf, _arr, obj_size * array_size);
+  return v;
+}
+
+bool vec_insert(vec_t *vec, const vec_data_t _data, size_t idx) {
+  assert(vec && _data && vec->size > idx);
+  if (vec->cap == vec->size) {
+    if (!vec_enlarge(vec, vec->cap * 2)) return false;
+  }
+  memmove(vec->buf + vec->obj_size * (idx + 1), vec->buf + vec->obj_size * idx,
+          vec->size - idx);
+  memcpy(vec->buf + vec->obj_size * idx, _data, vec->obj_size);
+  vec->size++;
+  return true;
+}
+
+bool vec_sort(vec_t *vec, int (*cmp_fn)(const void *, const void *)) {
+  assert(vec);
+  if (vec->size == 0) return false;
+  qsort(vec->buf, vec->size, vec->obj_size, cmp_fn);
+  return true;
+}
+
+bool vec_del_at(vec_t *vec, const size_t idx) {
+  assert(vec && vec->size > idx);
+  if (idx == vec->size - 1) {
+    return vec_del_top(vec);
+  }
+  char *buf = vec->buf;
+  memmove(buf + vec->obj_size * idx, buf + vec->obj_size * (idx + 1),
+          vec->obj_size * (vec->size - idx - 1));
+  vec->size--;
+  return true;
+}
+
+const vec_data_t vec_at(const vec_t *vec, const size_t idx) {
+  assert(vec && vec->size > idx);
+  char *ptr = vec->buf + vec->obj_size * idx;
+  return (void *)ptr;
+}
+
+const vec_data_t vec_top(const vec_t *vec) {
+  return vec_at(vec, vec->size - 1);
+}
+
+bool vec_reverse(vec_t *v) {
+  assert(v);
+  if (v->size == 0) return false;
+  size_t start = 0;
+  size_t end = v->size - 1;
+  char tmp[v->obj_size];
+  while (start < end) {
+    memcpy(tmp, v->buf + start * v->obj_size, v->obj_size);
+    memcpy(v->buf + start * v->obj_size, v->buf + end * v->obj_size,
+           v->obj_size);
+    memcpy(v->buf + end * v->obj_size, tmp, v->obj_size);
+    start++;
+    end--;
+  }
+  return true;
+}
+
+#endif
